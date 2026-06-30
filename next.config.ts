fbcd84c2adb9ae35524def2810cddffbb4dbcc5f
stephenabv <stephen.abueva@gmail.com>
2026-06-30 15:57:22 +0800

    Fix issue on footer.

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,7 @@ const nextConfig: NextConfig = {
   turbopack: {
     rules: {
       "*.less": {
+        condition: { not: { path: /\.module\.less$/ } },
         loaders: [
           {
             loader: "less-loader",
